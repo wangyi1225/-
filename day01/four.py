4e36525219d685c69b828313ef4a6d3e428e8f49
wangyi1225 <49989548+wangyi1225@users.noreply.github.com>
2022-09-20 07:49:25 +0800

    Add files via upload

---
+++ day01/four.py
@@ -0,0 +1,19 @@
+'''
+a = 10
+b = 3
+print(a%b)	# xxx是否可以被xxxx整除
+
+
+num = input("请输入一个数:")
+num = int(num)
+if num % 3 == 0:
+	print("是三的倍数")
+else:
+	print("不是三的倍数")
+	
+'''
+a = 10
+b = 3
+print(a//b)	# 取整
+
+
